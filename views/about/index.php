3d4ec0bb7dbde0575a136b3c7655d9da11846e2e
Matthew Peterson <mrpeterson2@gmail.com>
2026-03-29 08:06:58 -0700

    Update links and description from original 2015 listing

--- views/about/index.php
+++ views/about/index.php
@@ -9,16 +9,16 @@ $this->setLayoutVar('pageTitle', 'About');
   <li>Author: Matthew Peterson <a href="mailto:petersm3@oregonstate.edu">petersm3@oregonstate.edu</a></li>
   <li>Project: Image and cultural properties browser
       <ul>
-          <li>Emulating front-end interface of <a target="_blank" href="http://buildingoregon.org/">http://buildingoregon.org/</a></li>
+          <li>Emulating the fucntionality of <a target="_blank" href="https://oregondigital.org/collections/building-or">Building Oregon</a> (circa 2015)</li>
       </ul>
   </li>
-  <li>GitHub: <a target="_blank" href="https://github.com/petersm3/browser">https://github.com/petersm3/browser</a></li>
+  <li>GitHub: <a target="_blank" href="https://github.com/petersm3/browser">https://github.com/petersm3/browser-rewrite</a></li>
   <li>Technologies used:
       <ul>
          <li><a target="_blank" href="http://php.net/">PHP</a></li>
          <li><a target="_blank" href="http://php.net/manual/en/book.pdo.php">PDO</a></li>
          <li><a target="_blank" href="https://www.mysql.com/">MySQL</a></li>
-         <li><a target="_blank" href="http://lightvc.org/">LightVC</a></li>
+         <li><a target="_blank" href="https://github.com/awbush/lightvc">LightVC</a></li>
          <li><a target="_blank" href="http://getbootstrap.com/">Bootstrap</a></li>         
          <li><a target="_blank" href="http://dummyimage.com/">Dynamic Dummy Image Generator</a></li>
       </ul>
